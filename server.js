6433467bbcac6bc784247b724101945504898af1
dosmaster4life <jefferysuperman@gmail.com>
2022-09-12 15:53:56 -0600

    Inital server.js file with default route and port

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!')); // This is the default route
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)); // This is the default port
