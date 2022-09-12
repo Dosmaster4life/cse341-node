8de57bf7d61c8b87dd9b79a7981931986b664ea4
dosmaster4life <jefferysuperman@gmail.com>
2022-09-12 15:55:16 -0600

    Changed response to display a name

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!')); // This is the default route
+app.get('/', (req, res) => res.send('Stewart!')); // This is the default route
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); // This is the default port
