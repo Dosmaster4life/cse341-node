c7b870e4e71d4c51e21696e2643803f736858057
dosmaster4life <jefferysuperman@gmail.com>
2022-09-13 16:01:42 -0600

    bug fixes to remove favicon.ico error

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
     res.send('Stewart');
 });
+
 module.exports = routes;
--- server.js
+++ server.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // set our port 
 
-app.use('/',require('./routes'));
+app.use('/',require('./routes')); // set the routes
+app.get('/favico.ico', (req, res) => { // removes the error for favicon.ico missing in some browsers.
+    res.sendStatus(404); // We don't need to send anything back.
+});
 app.listen(port, () => console.log(`Running on port: ${port}!`)); // This is the default port
