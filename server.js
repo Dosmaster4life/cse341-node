0282c4a680b41e1b57ad1a331080fed04c5a64a5
dosmaster4life <jefferysuperman@gmail.com>
2022-09-12 18:46:01 -0600

    minor code change

--- server.js
+++ server.js
@@ -3,6 +3,4 @@ const app = express();
 const port = process.env.PORT || 3000; // set our port 
 
 app.use('/',require('./routes'));
-app.get('/', (req, res) => res.send('Hello World!')); // This is the default route
-
 app.listen(port, () => console.log(`Running on port: ${port}!`)); // This is the default port
